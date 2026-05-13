f0c8af573a7ec1ac018cc1a20e894e0a306fb206
abbyxianggu <abbyxianggu@gmail.com>
2026-05-13 18:34:40 +1000

    hehe

--- 2026 Software Engineering Project Code/PITCHD/__init__.py
+++ 2026 Software Engineering Project Code/PITCHD/__init__.py
@@ -10,6 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'lololol'
     app.config['SQL_ALCHEMY_DATABASE_URI'] =f'sqlite://{DB_name}'
+    
     db.init(app)
     
     from .page import page
@@ -19,8 +20,12 @@ def create_app():
     app.register_blueprint(page, url_prefix = '/')
     app.register_blueprint(authentication, url_prefix = '/')    
     
+    from .database import User, Task
+    create_database(app)
     return app
+
+
 def create_database(app):
-    if not os.path.exist('/'+ DB_name):
+    if not os.path.exists('/'+ DB_name):
         db.create_all(app=app)
         print("created database")
