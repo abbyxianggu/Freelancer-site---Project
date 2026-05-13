d18f1b75174d17a821896d214f15c6d0f2fdc4a2
abbyxianggu <abbyxianggu@gmail.com>
2026-05-13 18:29:09 +1000

    yeah

--- 2026 Software Engineering Project Code/PITCHD/__init__.py
+++ 2026 Software Engineering Project Code/PITCHD/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os 
 
-"""
 db = SQLAlchemy()
 DB_name = "database.db"
-"""
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'lololol'
+    app.config['SQL_ALCHEMY_DATABASE_URI'] =f'sqlite://{DB_name}'
+    db.init(app)
     
     from .page import page
     
@@ -18,3 +20,7 @@ def create_app():
     app.register_blueprint(authentication, url_prefix = '/')    
     
     return app
+def create_database(app):
+    if not os.path.exist('/'+ DB_name):
+        db.create_all(app=app)
+        print("created database")
